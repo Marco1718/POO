595d3f2aa93175f73b1de75cde7dd63a06cfc51a
Marco1718 <jhongarcianavarro75783@gmail.com>
2025-03-03 16:06:28 -0600

    Desarrollo del proyecto fase 1 Tiene la posibilidad de diferentes menus dependiendo el usuario, puedes retirar libros, devolverlos, ver el top de los mejores y peores usuarios y el top de los libros mas y menos populares

---
+++ poo/src/main/java/edu/marco/garcia/actividades/Proyecto/models/Users.java
@@ -0,0 +1,80 @@
+package edu.marco.garcia.actividades.proyecto.models;
+
+public class Users {
+
+    private String username;
+    private String password;
+    private int age;
+    private String name;
+    private String tipo;
+    private int vencimientos;
+    private int entregados;
+
+    public Users(String username, String password, int age, String name, String tipo) {
+        setUsername(username);
+        setPassword(password);
+        setAge(age); 
+        setName(name);
+        setTipo(tipo);
+        vencimientos=0;
+        entregados=0;
+    }
+
+    public int getVencimientos() {
+        return vencimientos;
+    }
+
+    public int getEntregados() {
+        return entregados;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setTipo(String tipo) {
+        
+        this.tipo = tipo;
+    }
+
+    public void setVencimientos(int vencimientos) {
+        this.vencimientos = vencimientos;
+    }
+
+    public void setEntregados(int entregados) {
+        this.entregados = entregados;
+    }
+}
